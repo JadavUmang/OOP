e430114d6f7214bcdf4010e7532cabe06a313387
Umang <jadavumang160@gmail.com>
2024-03-29 13:34:17 +0530

    updated proc_15

--- Prac_15.java
+++ Prac_15.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Prac_15 {
     int roll;
     double cgpa;
@@ -9,6 +11,16 @@ public class Prac_15 {
         this.cgpa = cgpa;
     }
 
+    public Prac_15() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Roll No Of Student : ");
+        roll  = sc.nextInt();
+        System.out.print("Enter Name Of Student : ");
+        name  = sc.next();
+        System.out.print("Enter CGPA Of Student : ");
+        cgpa  = sc.nextDouble();
+    }
+
     public void display(Prac_15 A) {
         if (A.cgpa > 5) {
             System.out.println("Student " + A.name + " Having Roll no " + A.roll + " Is Pass.");
@@ -18,9 +30,9 @@ public class Prac_15 {
     }
 
     public static void main(String[] args) {
-        Prac_15 s1 = new Prac_15(1, "Umang", 9.5);
-        Prac_15 s2 = new Prac_15(2, "Jenish", 4.99);
+        Prac_15 s1 = new Prac_15();
         s1.display(s1);
+        Prac_15 s2 = new Prac_15();
         s2.display(s2);
     }
 }
